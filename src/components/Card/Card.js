188ffa415ec7259936eb6a886a7ca98e9c1a7b6f
kiryu <13matheusilva@gmail.com>
2024-06-03 20:49:43 -0300

    card style add

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,7 +3,7 @@ import style from './Card.module.css'
 const Card = (prop) => {
     return (
         <div class={style.card}>
-            <p class={style.fishName}>{prop.fish}</p>
+            <p class={style.fish}>{prop.fish}</p>
             <img src={prop.src} alt={prop.alt}></img>
             <div>
                 <p class={style.size}><i class="fa-solid fa-fish-fins"></i> {prop.size}</p>
